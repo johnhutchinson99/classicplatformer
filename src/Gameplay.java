ab4e467374b799dcbcdcfa2aa502104bb1f6e663
michelle-n <michelle.nguyen3@ucalgary.ca>
2019-07-18 16:01:49 -0600

    Added skeletons for Gameplay, Entity classes.
    Added skeletons for gameplay, entity classes.
    
    Removed "Test" class (this class was a HelloWorld program that was previously used to test that Github works on our laptops).

---
+++ src/Gameplay.java
@@ -0,0 +1,22 @@
+
+public class Gameplay {
+
+	public static void main(String[] args) {
+		
+		// Game intro / title
+		
+		// Ask for player name
+		
+		// Print or announce gameplay begins
+		
+		// Loop while game active / alive
+			// Draw world
+		
+			// Prompt for user input
+		
+		
+		// Game over (lose or win)
+		
+	}
+
+}
