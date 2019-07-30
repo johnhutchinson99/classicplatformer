ac08e21ae10ada3978094dd6dae6933db5244264
PohwaranNNNN <52805470+PohwaranNNNN@users.noreply.github.com>
2019-07-30 00:03:30 -0600

    Add files via upload

--- src/TrapType1.java
+++ src/TrapType1.java
@@ -1,47 +1,38 @@
 
-public class TrapType1 extends Enemy {  //This trap is move up and move down(underground)
+public class TrapType1 extends EnemyGUI {  //This trap is move up and move down(underground)
 	
-	private boolean isUp = true;
-	private int upMax = 0;
-	private int downMax = 0;
+	private boolean isUp;
+	private int upMax;
+	private int downMax;
 	
 	//Thread t = new Thread();
 
 	@Override
 	public void move() {
-		if (this.isUp) {
-			this.y -= height;
-		} else {
-			this.y += height;
+		if (this.getEnemyYCoord() >= this.upMax) {
+			isUp = false;
+		}
+		if (this.getEnemyYCoord() <= this.downMax) {
+			isUp = true;
 		}
 		
-		if (this.isUp && this.y == this.upMax) {
-			this.isUp = false;
+		if (isUp == true) {
+			setXCoord(getEnemyYCoord()+1);
 		}
-		if (!this.isUp && this.y == this.downMax) {
-			this.isUp = true;
+		if (isUp == false) {
+			setXCoord(getEnemyYCoord()-1);
 		}
-
 	}
-	
-	/*
-	@Override
-	public void run() {
-		move();
-	}
-	*/
-	
-	public TrapType1(int x, int y, boolean isUp, int upMax, int downMax) {
-		super(x,y);
-		this.isUp = isUp;
-		this.upMax = upMax;
-		this.downMax = downMax;	
-	}
+		
+		public TrapType1(World newWorld,int enemyXCoord,int enemyYCoord,boolean isUp,int upMax) {
+			super(newWorld, enemyXCoord,enemyYCoord);
+			this.isUp = isUp;
+			this.upMax = upMax;
+		}
 	
 	public TrapType1(TrapType1 toCopy){
-		super(toCopy.getX(),toCopy.getY());
+		super(toCopy.getWorld(), toCopy.getEnemyXCoord(), toCopy.getEnemyYCoord());
 		this.isUp = toCopy.isUp;
 		this.upMax = toCopy.upMax;
-		this.downMax = toCopy.downMax;	
 	}
 }
