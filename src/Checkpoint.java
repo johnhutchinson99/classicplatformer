79813a8e85be1c0074d34bb5a5382340fc3adce3
johnhutchinson99 <john.hutchinson@ucalgary.ca>
2019-08-06 16:55:35 -0600

    Added checkpoint object
    Feel free to change the name

---
+++ src/Checkpoint.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Colliding with a Checkpoint object leads to a game win
+ *
+ */
+public class Checkpoint extends Entity{
+
+	public Checkpoint(int newWidth, int newHeight) {
+		super(newWidth,newHeight);
+	}
+
+}
